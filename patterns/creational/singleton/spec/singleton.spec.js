af5ce9fdedb670249b3c6e9bc6819d404cb1f4a9
instanceofpro <instanceofpro@gmail.com>
2014-02-05 15:01:58 +0600

    Добавил еще один тестик для синглтона

--- patterns/creational/singleton/spec/singleton.spec.js
+++ patterns/creational/singleton/spec/singleton.spec.js
@@ -7,6 +7,10 @@ describe('Синглтон / Singleton', function () {
         secondIns = Singleton.getInstance();
     });
 
+    it('Проверка метода, возвращающего инстанс синглтона', function () {
+        expect(Singleton.getInstance).to.exist;
+    });
+
     it('Обе переменные ссылаются на один и тот же объект', function () {
         expect(firstIns).to.equal(secondIns);
     });
