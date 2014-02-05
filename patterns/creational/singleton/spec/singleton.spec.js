0352a689847ce4b60fb8fbcf9026dd06ddd42175
instanceofpro <instanceofpro@gmail.com>
2014-02-05 15:08:14 +0600

    Добавил еще один тестик для синглтона

--- patterns/creational/singleton/spec/singleton.spec.js
+++ patterns/creational/singleton/spec/singleton.spec.js
@@ -5,13 +5,21 @@ describe('Синглтон / Singleton', function () {
     before(function () {
         firstIns = Singleton.getInstance();
         secondIns = Singleton.getInstance();
+
+        firstIns.toString = function() {
+            return '[object Singleton]';
+        };
     });
 
-    it('Проверка метода, возвращающего инстанс синглтона', function () {
+    it('Метод, возвращающий инстанс синглтона должен существовать', function () {
         expect(Singleton.getInstance).to.exist;
     });
 
     it('Обе переменные ссылаются на один и тот же объект', function () {
         expect(firstIns).to.equal(secondIns);
+    });
+
+    it('Метода toString должен существовать', function () {
+        expect(secondIns.toString).to.exist;
     });
 });
