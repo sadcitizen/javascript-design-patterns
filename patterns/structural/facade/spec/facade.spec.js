02c1aabf470c90a5f55cd4085b55b411b0062fa0
instanceofpro <instanceofpro@gmail.com>
2014-02-06 16:58:03 +0600

    Тесты на проверку инстансов составляющих фасада

--- patterns/structural/facade/spec/facade.spec.js
+++ patterns/structural/facade/spec/facade.spec.js
@@ -1,5 +1,34 @@
-describe('', function () {
+describe('Фасад / Facade', function () {
 
-    it('Первый тест');
+    var market, cash, cart, warehouse;
+
+    before(function () {
+        market = new Supermarket(['milk', 'bread', 'meat'], 100);
+        cash = new Cash(100);
+        cart = new Cart();
+        warehouse = new Warehouse();
+    });
+
+    describe("Прверка инстансов", function () {
+
+        it("Supermarket", function () {
+            expect(market).to.be.an.instanceOf(Supermarket);
+        });
+
+        it("Cash", function () {
+            expect(cash).to.be.an.instanceOf(Cash);
+        });
+
+        it("Cart", function () {
+            expect(cart).to.be.an.instanceOf(Cart);
+        });
+
+        it("Warehouse", function () {
+            expect(warehouse).to.be.an.instanceOf(Warehouse);
+        });
+
+    });
+
+
 
 });
