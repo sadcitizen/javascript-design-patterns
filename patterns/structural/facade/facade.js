8b0e0bf747fe358299e10d171277a97fab7d370a
instanceofpro <instanceofpro@gmail.com>
2014-02-10 11:01:12 +0600

    Тесты на проверку инстансов составляющих фасада

--- patterns/structural/facade/facade.js
+++ patterns/structural/facade/facade.js
@@ -14,10 +14,12 @@ function Cart() {
 
 Cart.prototype.add = function (item) {
     this._items.push(item);
+    return this;
 };
 
 Cart.prototype.clean = function () {
     this._items = [];
+    return this;
 };
 
 Cart.prototype.getCost = function () {
@@ -26,36 +28,48 @@ Cart.prototype.getCost = function () {
     for(; i < length; i++){
         sum += this._items[i];
     }
+
+    return sum;
 };
 
-function Warehouse() {
-    this._goods = {
-        milk: { count: 200, price: 6 },
-        bread: { count: 150, price: 5 },
-        meat: { count: 120, price: 20 }
-    };
+function Warehouse(goods) {
+    this._goods = goods;
+
+    /** Goods sample
+     * {
+     *   milk: { count: 200, price: 6 },
+     *   bread: { count: 150, price: 5 },
+     *   meat: { count: 120, price: 20 }
+     * }
+     * */
 }
 
-Warehouse.prototype.get = function (item) {
+Warehouse.prototype.getItem = function (item) {
     this._goods[item].count--;
     return this._goods[item].price;
 };
 
+Warehouse.prototype.getItemCount = function (item) {
+    return this._goods[item].count;
+};
+
+
+
 // Фасад
-function Supermarket(shoppingList, money) {
+function Supermarket(shoppingList, money, goods) {
     this._list = shoppingList;
     this._money = money;
 
     this._cash = new Cash(this._money);
     this._cart = new Cart();
-    this._store = new Warehouse();
+    this._store = new Warehouse(goods);
 }
 
 Supermarket.prototype.goShopping = function () {
     var i = 0, length = this._list.length;
 
     for(;i < length; i++) {
-        this._cart.add(this._store.get(this._list[i]));
+        this._cart.add(this._store.getItem(this._list[i]));
     }
 
     return this._cash.pay(this._cart.getCost());
--- patterns/structural/facade/spec/facade.spec.js
+++ patterns/structural/facade/spec/facade.spec.js
@@ -1,15 +1,19 @@
 describe('Фасад / Facade', function () {
 
-    var market, cash, cart, warehouse;
-
-    before(function () {
-        market = new Supermarket(['milk', 'bread', 'meat'], 100);
-        cash = new Cash(100);
-        cart = new Cart();
-        warehouse = new Warehouse();
-    });
-
     describe("Прверка инстансов", function () {
+
+        var market, cash, cart, warehouse;
+
+        before(function () {
+            market = new Supermarket(['milk', 'bread', 'meat'], 100);
+            cash = new Cash(100);
+            cart = new Cart();
+            warehouse = new Warehouse({
+                milk: { count: 200, price: 6 },
+                bread: { count: 150, price: 5 },
+                meat: { count: 120, price: 20 }
+            });
+        });
 
         it("Supermarket", function () {
             expect(market).to.be.an.instanceOf(Supermarket);
@@ -26,9 +30,89 @@ describe('Фасад / Facade', function () {
         it("Warehouse", function () {
             expect(warehouse).to.be.an.instanceOf(Warehouse);
         });
-
     });
 
+    describe('Cash', function () {
 
+        var cash;
+
+        before(function () {
+            cash = new Cash(100);
+        });
+
+        it('Cash.pay', function() {
+            expect(cash.pay(200)).to.be.false;
+            expect(cash.pay(90)).to.be.true;
+        });
+    });
+
+    describe('Cart', function () {
+
+        var cart;
+
+        before(function () {
+            cart = new Cart();
+        });
+
+        it('Cart.add', function () {
+            cart
+                .add(100)
+                .add(100)
+                .add(100)
+                .add(100);
+
+            expect(cart.getCost()).to.equal(400);
+        });
+
+        it('Cart.clean', function () {
+            cart.clean();
+
+            expect(cart.getCost()).to.equal(0);
+        });
+    });
+
+    describe('Warehouse', function () {
+
+        var count, warehouse;
+
+        beforeEach(function () {
+            warehouse = new Warehouse({
+                milk: { count: 200, price: 6 },
+                bread: { count: 150, price: 5 },
+                meat: { count: 120, price: 20 }
+            });
+
+            count = warehouse.getItemCount('milk');
+        });
+
+        it('Warehouse.getItem', function () {
+            expect(warehouse.getItem('milk')).to.equal(6);
+            expect(count - warehouse.getItemCount('milk')).to.equal(1);
+        });
+
+        it('Warehouse.getItemCount', function () {
+            expect(warehouse.getItemCount('milk')).to.equal(200);
+            expect(warehouse.getItemCount('bread')).to.equal(150);
+            expect(warehouse.getItemCount('meat')).to.equal(120);
+        });
+    });
+
+    describe('Supermarket', function () {
+
+        var supermarket;
+
+        before(function () {
+            supermarket = new Supermarket(['milk', 'bread', 'meat'], 100, {
+                milk: { count: 200, price: 6 },
+                bread: { count: 150, price: 5 },
+                meat: { count: 120, price: 20 }
+            });
+        });
+
+        it('Supermarket.goShopping', function () {
+            expect(supermarket.goShopping()).to.be.true;
+        });
+
+    });
 
 });
