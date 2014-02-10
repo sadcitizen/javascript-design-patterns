f5b74fa3fb6e96dbda7b46fa4a65229930124fe2
instanceofpro <instanceofpro@gmail.com>
2014-02-10 15:14:03 +0600

    Правки декоратора

--- patterns/structural/decorator/spec/decorator.spec.js
+++ patterns/structural/decorator/spec/decorator.spec.js
@@ -7,27 +7,37 @@ describe('Декоратор / Decorator или Оболочка / Wrapper', fun
         vine = new Vine('Chateau Rieussec', 'white', 250, 'Sauternes 1-er Grand Cru Classe AOC');
     });
 
-    it("Проверка инстанса Beverage", function () {
-        expect(beverage).to.be.an.instanceOf(Beverage);
+    describe('Инстансы', function () {
+        it("Beverage", function () {
+            expect(beverage).to.be.an.instanceOf(Beverage);
+            expect(beverage.getCost).to.be.a('function');
+            expect(beverage.getDescription).to.be.a('function');
+        });
+
+        it("Vine", function () {
+            expect(vine).to.be.an.instanceOf(Vine);
+            expect(vine.getCost).to.be.a('function');
+            expect(vine.getDescription).to.be.a('function');
+            expect(vine.getName).to.be.a('function');
+            expect(vine.getColor).to.be.a('function');
+        });
     });
 
-    it("Проверка инстанса Vine", function () {
-        expect(vine).to.be.an.instanceOf(Vine);
-    });
+    describe('Методы', function () {
+        it("Метод getName()", function () {
+            expect(vine.getName()).to.equal('Chateau Rieussec');
+        });
 
-    it("Метод getName()", function () {
-        expect(vine.getName()).to.equal('Chateau Rieussec');
-    });
+        it("Метод getColor()", function () {
+            expect(vine.getColor()).to.equal('white');
+        });
 
-    it("Метод getColor()", function () {
-        expect(vine.getColor()).to.equal('white');
-    });
+        it("Метод getCost()", function () {
+            expect(vine.getCost()).to.equal(250);
+        });
 
-    it("Метод getCost()", function () {
-        expect(vine.getCost()).to.equal(250);
-    });
-
-    it("Метод getName()", function () {
-        expect(vine.getDescription()).to.equal('Sauternes 1-er Grand Cru Classe AOC');
+        it("Метод getName()", function () {
+            expect(vine.getDescription()).to.equal('Sauternes 1-er Grand Cru Classe AOC');
+        });
     });
 });
