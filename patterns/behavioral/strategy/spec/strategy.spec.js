c814067ffff74d1984d3179d45f4c4be902d592f
instanceofpro <instanceofpro@gmail.com>
2014-02-10 16:57:11 +0600

    Добавил стратегию

--- patterns/behavioral/strategy/spec/strategy.spec.js
+++ patterns/behavioral/strategy/spec/strategy.spec.js
@@ -1,17 +1,41 @@
 describe('Стратегия / Strategy', function () {
 
-    var strategy;
+    var calc;
 
     beforeEach(function () {
-        strategy = new Strategy();
+        calc = new Calculator();
     });
 
     describe('Инстанс', function () {
 
         it('Strategy', function () {
-            expect(strategy).to.be.instanceOf(Strategy);
+            expect(calc).to.be.instanceOf(Calculator);
+        });
+
+        it('Методы', function () {
+            expect(calc.set).to.be.a('function');
+            expect(calc.execute).to.be.a('function');
+        });
+    });
+
+    describe('Поведение', function () {
+
+        it('Calculator.execute', function () {
+            expect(calc.execute('add', 4, 5)).to.equal(9);
+            expect(calc.execute('subtract', 4, 5)).to.equal(-1);
+            expect(calc.execute('multiply', 4, 5)).to.equal(20);
+            expect(calc.execute('divide', 40, 5)).to.equal(8);
+        });
+
+        it('Calculator.set', function () {
+
+            calc.set('pow', function(x, y) {
+                return Math.pow(x, y);
+            });
+
+            expect(calc.execute('pow', 15, 2)).to.equal(225);
+
         });
 
     });
-
 });
--- patterns/behavioral/strategy/strategy.js
+++ patterns/behavioral/strategy/strategy.js
@@ -1,12 +1,46 @@
-var Strategy = (function () {
+/**
+ * Калькулятор - стратегия
+ * */
+var Calculator = (function () {
 
-    function Strategy() {
+    function Calculator() {
 
+        /**
+         * Встроенные алгоритмы
+         * */
+        this._methods = {
+            add: function (x, y) {
+                return x + y;
+            },
+
+            subtract: function(x, y) {
+                return x - y;
+            },
+
+            multiply: function (x, y) {
+                return x * y;
+            },
+
+            divide: function (x, y) {
+                return x / y;
+            }
+        };
     }
 
-    Strategy.prototype.method = function () {
-
+    /**
+     * Метод, который позволяет добавить новый метод в калькулятор
+     * */
+    Calculator.prototype.set = function (name, method) {
+        if (this._methods[name] === undefined) {
+            this._methods[name] = method;
+        }
     };
 
-    return Strategy;
+    Calculator.prototype.execute = function (name, x, y) {
+        if (this._methods[name]) {
+            return this._methods[name](x, y);
+        }
+    };
+
+    return Calculator;
 })();
