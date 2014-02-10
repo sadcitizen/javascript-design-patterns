5676adfa11dde3323a24cec3393bce7a10d2c83c
instanceofpro <instanceofpro@gmail.com>
2014-02-10 12:35:13 +0600

    Рефакторинг итератора

--- patterns/behavioral/iterator/spec/iterator.spec.js
+++ patterns/behavioral/iterator/spec/iterator.spec.js
@@ -8,63 +8,80 @@ describe('Итератор / Iterator или Курсор / Cursor', function ()
         sum = 0;
     });
 
-    it("Проверка инстанса", function () {
-        expect(iterated).to.be.an.instanceOf(Iterator);
-    });
-    
-    it('Текущий элемент', function () {
-        expect(iterated.current()).to.equal(0);
-    });
+    describe('Проверка объекта', function () {
 
-    it("Следующий элемент", function () {
-        expect(iterated.next()).to.equal(1);
-    });
-
-    it("Последний элемент", function () {
-        expect(iterated.last()).to.equal(9);
-    });
-
-    it("Предыдущий элемент", function () {
-        expect(iterated.previous()).to.equal(8);
-    });
-
-    it("Проход по всем элементам", function () {
-        iterated.reset();
-
-        iterated.each(function (item) {
-            sum += item;
+        it("Инстанс", function () {
+            expect(iterated).to.be.an.instanceOf(Iterator);
         });
 
-        expect(sum).to.equal(45);
+        it('Методы', function () {
+            expect(iterated.current).to.be.a('function');
+            expect(iterated.first).to.be.a('function');
+            expect(iterated.last).to.be.a('function');
+            expect(iterated.next).to.be.a('function');
+            expect(iterated.previous).to.be.a('function');
+            expect(iterated.reset).to.be.a('function');
+            expect(iterated.hasNext).to.be.a('function');
+            expect(iterated.hasPrevious).to.be.a('function');
+            expect(iterated.each).to.be.a('function');
+        });
     });
 
-    it("Есть ли следующий элемент", function () {
-        iterated.first();
+    describe('Работа методов', function () {
+        it('Текущий элемент', function () {
+            expect(iterated.current()).to.equal(0);
+        });
 
-        expect(iterated.hasNext()).to.be.true;
-    });
+        it("Следующий элемент", function () {
+            expect(iterated.next()).to.equal(1);
+        });
 
-    it("Есть ли предыдущий элемент", function () {
-        iterated.last();
+        it("Последний элемент", function () {
+            expect(iterated.last()).to.equal(9);
+        });
 
-        expect(iterated.hasPrevious()).to.be.true;
-    });
+        it("Предыдущий элемент", function () {
+            expect(iterated.previous()).to.equal(8);
+        });
 
-    it("Сброс курсора", function () {
-        iterated.reset();
+        it("Проход по всем элементам", function () {
+            iterated.reset();
 
-        expect(iterated.current()).to.equal(0);
-    });
+            iterated.each(function (item) {
+                sum += item;
+            });
 
-    it("Выход из диапазона справа", function () {
-        iterated.last();
+            expect(sum).to.equal(45);
+        });
 
-        expect(iterated.next()).to.be.undefined;
-    });
+        it("Есть ли следующий элемент", function () {
+            iterated.first();
 
-    it("Выход из диапазона слева", function () {
-        iterated.first();
+            expect(iterated.hasNext()).to.be.true;
+        });
 
-        expect(iterated.previous()).to.be.undefined;
+        it("Есть ли предыдущий элемент", function () {
+            iterated.last();
+
+            expect(iterated.hasPrevious()).to.be.true;
+        });
+
+        it("Сброс курсора", function () {
+            iterated.reset();
+
+            expect(iterated.current()).to.equal(0);
+        });
+
+        it("Выход из диапазона справа", function () {
+            iterated.last();
+
+            expect(iterated.next()).to.be.undefined;
+        });
+
+        it("Выход из диапазона слева", function () {
+            iterated.first();
+
+            expect(iterated.previous()).to.be.undefined;
+        });
     });
 });
