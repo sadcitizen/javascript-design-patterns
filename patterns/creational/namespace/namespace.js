5a8cf6155c8bbaeeb76ec98b3ab6f5288bf2d2a9
instanceofpro <instanceofpro@gmail.com>
2014-02-11 16:04:10 +0600

    Реализация паттерна Пространство имен

--- patterns/creational/namespace/namespace.js
+++ patterns/creational/namespace/namespace.js
@@ -1,3 +1,41 @@
 /**
- * 
+ * Объект приложения, внутри которого будут определяться неймспейсы
  * */
+var Namespace = (function () {
+
+    function Namespace() {
+    }
+
+    /**
+     * Метод, который создает неймспейс
+     * @param target {object} объект приложения, в котором создается неймспейс
+     * @param hash {string} строка неймспейса
+     * @param unit {(object|function)} модуль приложения
+     * */
+    Namespace.prototype.create = function (target, hash, unit) {
+        var parts = hash.split('.'),
+            length = parts.length,
+            i = 0,
+            current = target;
+
+        for (; i < length; i++) {
+            if (current[parts[i]] === undefined) {
+                current[parts[i]] = {};
+            }
+            current = current[parts[i]];
+        }
+
+        //TODO: Избавиться от _.extend()
+        if (typeof unit === 'function') {
+            _.extend(current, new unit());
+        }
+
+        if (typeof unit === 'object') {
+            _.extend(current, unit);
+        }
+
+        return current;
+    };
+
+    return new Namespace();
+})();
---
+++ patterns/creational/namespace/spec/namespace.spec.js
@@ -0,0 +1,51 @@
+describe('Пространство имен / Namespace', function () {
+
+    describe('Инстанс', function () {
+
+        it('Namespace', function () {
+            expect(Namespace).to.be.a('object');
+        });
+
+        it('Методы', function () {
+            expect(Namespace.create).to.exist;
+            expect(Namespace.create).to.be.a('function');
+        });
+    });
+
+    describe('Поведение', function () {
+
+        it('Создание неймспейсов', function () {
+            var App = App || {};
+
+            expect(Namespace.create(App, 'modules.login')).to.equal(App.modules.login);
+            expect(Namespace.create(App.modules, 'logout')).to.equal(App.modules.logout);
+            expect(Namespace.create(App.modules.logout, 'success', {})).to.equal(App.modules.logout.success);
+            expect(Namespace.create(App, 'modules.logout.fail', {})).to.equal(App.modules.logout.fail);
+        });
+
+        it('Создание неймспейсов с объектом', function () {
+            var AnotherApp = AnotherApp || {};
+
+            expect(Namespace.create(AnotherApp, 'modules.login', { foo: 'bar' })).to.equal(AnotherApp.modules.login);
+            expect(AnotherApp.modules.login).to.have.property('foo');
+        });
+
+        it('Создание неймспейсов с функциями-фабриками', function () {
+            var FnApp = FnApp || {};
+
+            expect(Namespace.create(FnApp, 'modules.login', function () {
+                return {
+                    foo: 'bar',
+                    hello: function () {
+                        return 'world'
+                    }
+                };
+            })).to.equal(FnApp.modules.login);
+
+            expect(FnApp.modules.login).to.have.property('foo');
+            expect(FnApp.modules.login).to.have.property('hello');
+            expect(FnApp.modules.login.hello).to.be.a('function');
+        });
+
+    });
+});
