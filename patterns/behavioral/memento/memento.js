b50b44b21c49ab65bea5b83478017cde5fa9600d
instanceofpro <instanceofpro@gmail.com>
2014-02-19 20:23:46 +0600

    Реализация паттерна Хранитель + тесты

--- patterns/behavioral/memento/memento.js
+++ patterns/behavioral/memento/memento.js
@@ -38,14 +38,12 @@ var Person = (function () {
  * */
 var Memento = (function () {
 
-    var jsonState;
-
     function Memento(state) {
-        jsonState = JSON.stringify(state);
+        this.jsonState = JSON.stringify(state);
     }
 
     Memento.prototype.getState = function () {
-        return jsonState;
+        return this.jsonState;
     };
 
     return Memento;
@@ -62,7 +60,6 @@ var Caretaker = (function (){
 
     Caretaker.prototype.set = function(memento) {
         this.mementos.push(memento);
-        return this;
     };
 
     Caretaker.prototype.get = function(index) {
@@ -70,26 +67,9 @@ var Caretaker = (function (){
         return this.mementos[isNaN(ind) ? this.mementos.length - 1 : ind];
     };
 
+    Caretaker.prototype.clean = function () {
+        this.mementos = [];
+    };
+
     return Caretaker;
 })();
-
-/**
- * Использование
- * */
-
-var john = new Person('John', '1600 Pennsylvania Ave NW, Washington, DC 20500');
-console.log(john.whereami()); // => '1600 Pennsylvania Ave NW, Washington, DC 20500'
-var johnCareTaker = new Caretaker();
-johnCareTaker.set(john.save());
-
-john.move('935 Pennsylvania Ave NW, Washington, District of Columbia 20535');
-console.log(john.whereami()); // => '935 Pennsylvania Ave NW, Washington, District of Columbia 20535'
-johnCareTaker.set(john.save());
-
-
-debugger;
-/*john.restore(johnCareTaker.get());
-console.log(john.whereami()); // => '1600 Pennsylvania Ave NW, Washington, DC 20500'
-
-john.restore(johnCareTaker.get(0));
-console.log(john.whereami()); // => '935 Pennsylvania Ave NW, Washington, District of Columbia 20535'*/
--- patterns/behavioral/memento/spec/memento.spec.js
+++ patterns/behavioral/memento/spec/memento.spec.js
@@ -1,8 +1,123 @@
 describe('Хранитель / Memento', function () {
 
-    var mem;
+    var person, caretaker, mem;
 
-    before(function () {
+    describe('Классы', function (){
+
+        before(function () {
+            person = new Person('John', 'Moscow');
+            caretaker = new Caretaker();
+            mem = new Memento({hello: 'world'});
+        });
+
+        it('Person является классом', function () {
+            expect(Person).to.be.a('function');
+            expect(person).to.be.an.instanceOf(Person);
+        });
+
+        it('Экземпляр класса Person имеет метод move()', function() {
+            expect(person.move).to.be.a('function');
+        });
+
+        it('Экземпляр класса Person имеет метод whereami()', function() {
+            expect(person.whereami).to.be.a('function');
+            expect(person.whereami()).to.equal('Moscow');
+        });
+
+        it('Экземпляр класса Person имеет метод save()', function() {
+            expect(person.save).to.be.a('function');
+            expect(person.save()).to.be.an.instanceOf(Memento);
+        });
+
+        it('Экземпляр класса Person имеет метод restore()', function() {
+            var save = person.save();
+            person.move('Tokyo');
+            person.restore(save);
+
+            expect(person.restore).to.be.a('function');
+            expect(person.whereami()).to.equal('Moscow');
+        });
+
+        it('Memento является классом', function () {
+            expect(Memento).to.be.a('function');
+            expect(mem).to.be.an.instanceOf(Memento);
+        });
+
+        it('Экземпляр класса Memento имеет метод getState()', function() {
+            expect(mem.getState).to.be.a('function');
+            expect(mem.getState()).to.equal('{"hello":"world"}');
+        });
+
+        it('Caretaker является классом', function () {
+            expect(Caretaker).to.be.a('function');
+            expect(caretaker).to.be.an.instanceOf(Caretaker);
+        });
+
+        it('Экземпляр класса Memento имеет метод set()', function() {
+            expect(caretaker.set).to.be.a('function');
+        });
+
+        it('Экземпляр класса Memento имеет метод get()', function() {
+            expect(caretaker.get).to.be.a('function');
+        });
+
+        it('Экземпляр класса Memento имеет метод clean()', function() {
+            expect(caretaker.clean).to.be.a('function');
+        });
+    });
+
+    describe('Поведение', function () {
+
+        beforeEach(function () {
+            person = new Person('John', 'Moscow');
+            caretaker = new Caretaker();
+        });
+
+        it('Сохраненное и восстановленное состояния должны быть идентичными', function () {
+            var oldAddress = person.whereami();
+
+            caretaker.set(person.save());
+            person.restore(caretaker.get(0));
+
+            var newAddress = person.whereami();
+
+            expect(oldAddress === newAddress).to.be.true;
+        });
+
+        it('Множественное сохранение состояний', function () {
+
+            caretaker.set(person.save());
+
+            person.move('New York');
+            caretaker.set(person.save());
+
+            person.move('Tokyo');
+            caretaker.set(person.save());
+
+            person.move('London');
+            caretaker.set(person.save());
+
+            person.move('Paris');
+            caretaker.set(person.save());
+
+
+
+            person.restore(caretaker.get(0));
+            expect(person.whereami()).to.equal('Moscow');
+
+            person.restore(caretaker.get(1));
+            expect(person.whereami()).to.equal('New York');
+
+            person.restore(caretaker.get(2));
+            expect(person.whereami()).to.equal('Tokyo');
+
+            person.restore(caretaker.get(3));
+            expect(person.whereami()).to.equal('London');
+
+            person.restore(caretaker.get());
+            expect(person.whereami()).to.equal('Paris');
+
+        });
 
     });
 
