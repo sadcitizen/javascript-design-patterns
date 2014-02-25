f432695928049132c33f7cc82ebce0623456dd40
instanceofpro <instanceofpro@gmail.com>
2014-02-25 14:25:19 +0600

    Удаление обработчиков с учетом вложенности

--- patterns/behavioral/mediator/mediator.js
+++ patterns/behavioral/mediator/mediator.js
@@ -31,11 +31,12 @@ var Mediator = (function () {
      * @return {object} Ссылка на объект-медиатор для цепочки вызовов
      * */
     Mediator.prototype.on = function (channel, handler, context) {
-
-        this.namespace(channel).handlers.push({
-            fn: handler,
-            context: context
-        });
+        if (channel.length) {
+            this.namespace(channel).handlers.push({
+                fn: handler,
+                context: context
+            });
+        }
 
         return this;
     };
@@ -49,6 +50,14 @@ var Mediator = (function () {
      * @return {object} Ссылка на объект-медиатор для цепочки вызовов
      */
     Mediator.prototype.off = function(channel, withNested) {
+        if (this.has(channel)) {
+            var channel = this.namespace(channel);
+            channel.handlers = [];
+
+            if (withNested) {
+                channel.nested = {};
+            }
+        }
 
         return this;
     };
@@ -72,6 +81,18 @@ var Mediator = (function () {
      * @return {object} Ссылка на объект-медиатор для цепочки вызовов
      */
     Mediator.prototype.trigger = function(channel, data) {
+        if (this.has(channel)) {
+            var handlers = this.namespace(channel).handlers,
+                length = handlers.length,
+                i = 0;
+
+            for (; i < length; i++) {
+                handlers[i].fn.call(handlers[i].context, data);
+            }
+
+        } else {
+            throw new Error('The channel does not exist!');
+        }
 
         return this;
     };
@@ -83,8 +104,22 @@ var Mediator = (function () {
      * @return {object} Ссылка на объект-медиатор для цепочки вызовов
      */
     Mediator.prototype.clean = function (){
-        handlers = {};
+        storage = {};
         return this;
+    };
+
+    /**
+     * Проверка существования определенного неймспейса
+     *
+     * @this {Mediator}
+     * @param {string} channel Канал, который будет проверяться на наличие
+     * @return {boolean} Истина/ложь
+     *
+     * */
+    Mediator.prototype.has = function (channel) {
+        if (!channel.length) return false;
+
+        return true;
     };
 
     /**
@@ -142,46 +177,62 @@ var m = new Mediator();
  * Добавляем обработчик
  * */
 m.on('hello', function (data) {
-    console.log('Эти данные пришли из медиатора: ' + data.name);
+    console.log('Эти данные пришли из медиатора: hello -> ' + data.name);
 }, this);
 
 m.on('hello:world', function(data) {
-    console.log('Эти данные пришли из медиатора: ' + data.name);
-});
-
-m.on('hello:world', function(data) {
-    console.log('Эти данные пришли из медиатора: ' + data.name);
+    console.log('Эти данные пришли из медиатора: hello:world -> ' + data.name);
 });
 
 m.on('hello:another:world', function(data) {
-    console.log('Эти данные пришли из медиатора: ' + data.name);
+    console.log('Эти данные пришли из медиатора: hello:another:world -> ' + data.name);
 });
 
+m.off('hello');
+
 m.on('goodbye', function(data) {
-    console.log('Эти данные пришли из медиатора: ' + data.name);
+    console.log('Эти данные пришли из медиатора: goodbye -> ' + data.name);
 });
 
 m.on('goodbye:world', function(data) {
-    console.log('Эти данные пришли из медиатора: ' + data.name);
+    console.log('Эти данные пришли из медиатора: goodbye:world ->' + data.name);
 });
 
 m.on('goodbye:another:world', function(data) {
-    console.log('Эти данные пришли из медиатора: ' + data.name);
+    console.log('Эти данные пришли из медиатора: goodbye:another:world -> ' + data.name);
 });
 
 m.on('lorem:ipsum:dolor:sit:amet', function(data) {
-    console.log('Эти данные пришли из медиатора: ' + data.name);
+    console.log('Эти данные пришли из медиатора: lorem:ipsum:dolor:sit:amet -> ' + data.name);
 });
 
 m.on('lorem:ipsum', function(data) {
-    console.log('Эти данные пришли из медиатора: ' + data.name);
+    console.log('Эти данные пришли из медиатора: lorem:ipsum -> ' + data.name);
 });
 
+m.on('lorem:ipsum', function(data) {
+    console.log('Эти данные пришли из медиатора второй раз: lorem:ipsum -> ' + data.name);
+});
+
+m.off('lorem:ipsum:dolor', true);
 
 /**
  * Вызываем обработчик
  * */
 m.trigger('hello', { name: 'Аполлинарий Лаодикийский' });
 m.trigger('hello:world', { name: 'Вильгельм фон Каульбах' });
+/*m.trigger('', { name: 'Дени Дидро' });
+m.trigger('', { name: 'Фридрих Ницше' });
+m.trigger('', { name: 'Иммануил Кант' });
+m.trigger('', { name: 'Анри Пуанкаре' });
+m.trigger('', { name: 'Эварист Галуа' });
+m.trigger('', { name: 'Карл Вейерштрасс' });*/
+m.trigger('hello:another:world', { name: 'Якоб Бернулли' });
+m.trigger('goodbye', { name: 'Жан Даламбер' });
+m.trigger('goodbye:world', { name: 'Алан Тьюринг' });
+m.trigger('goodbye:another:world', { name: 'Норберт Винер' });
+m.trigger('lorem:ipsum:dolor:sit:amet', { name: 'Александр Ляпунов' });
+m.trigger('lorem:ipsum', { name: 'Артур Шопенгауэр' });
+
 
 console.log(m.getStorage());
