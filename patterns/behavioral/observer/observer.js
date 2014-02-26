ceb45037cb738d8e46d332c7ab1cb5c06fb372fa
instanceofpro <instanceofpro@gmail.com>
2014-02-26 12:07:05 +0600

    Добавил комментарии

--- patterns/behavioral/observer/observer.js
+++ patterns/behavioral/observer/observer.js
@@ -1,14 +1,27 @@
 var Observer = (function () {
-
+    /**
+     * Создает экземпляр наблюдателя
+     * @constructor
+     */
     function Observer() {
         this._handlers = [];
     }
 
+    /**
+     * Добавляет подписчика
+     * @param {function} handler Функция-подписчик
+     * @returns {Observer}
+     */
     Observer.prototype.subscribe = function (handler) {
         this._handlers.push(handler);
         return this;
     };
 
+    /**
+     * Удаляет подписчика
+     * @param {function} handler Функция-подписчик
+     * @returns {Observer}
+     */
     Observer.prototype.unsubscribe = function (handler) {
         var i = 0, length = this._handlers.length;
 
@@ -22,6 +35,11 @@ var Observer = (function () {
         return this;
     };
 
+    /**
+     * Инициирует запуск всех подписчиков (Публикует)
+     * @param {*} data Данные, которые будут переданы в подписчики
+     * @returns {Observer}
+     */
     Observer.prototype.publish = function (data) {
         var i = 0, length = this._handlers.length;
 
@@ -32,6 +50,10 @@ var Observer = (function () {
         return this;
     };
 
+    /**
+     * Удаляет подписчиков
+     * @returns {Observer}
+     */
     Observer.prototype.reset = function () {
         this._handlers = [];
         return this;
