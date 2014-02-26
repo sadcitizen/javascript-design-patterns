d6ed37f033be2e4104baaac331e5f91a22d360eb
instanceofpro <instanceofpro@gmail.com>
2014-02-26 11:43:41 +0600

    Добавил комментарии

--- patterns/behavioral/iterator/iterator.js
+++ patterns/behavioral/iterator/iterator.js
@@ -1,19 +1,32 @@
 var Iterator = (function () {
-
+    /**
+     * Создает экземпляр итератора
+     * @param {array} items Итерируемый массив
+     * @constructor
+     */
     function Iterator(items) {
         this._cursor = 0;
         this._items = items;
     }
 
+    /**
+     * Отдает текущий элемент массива
+     */
     Iterator.prototype.current = function () {
         return this._items[this._cursor];
     };
 
+    /**
+     * Сдвигает курсор в начальное положение и отдает первый элемент массива
+     */
     Iterator.prototype.first = function () {
         this.reset();
         return this.current();
     };
 
+    /**
+     * Сдвигает курсор на один шаг вперед и отдает элемент массива
+     */
     Iterator.prototype.next = function () {
         if (this.hasNext()) {
             this._cursor++;
@@ -21,6 +34,9 @@ var Iterator = (function () {
         }
     };
 
+    /**
+     * Сдвигает курсор на одни шаг назад и отдает элемент массива
+     */
     Iterator.prototype.previous = function () {
         if (this.hasPrevious()) {
             this._cursor--;
@@ -28,23 +44,42 @@ var Iterator = (function () {
         }
     };
 
+    /**
+     * Сдвигает курсор в конец и отдает последний элемент массива
+     * @returns {*}
+     */
     Iterator.prototype.last = function () {
         this._cursor = this._items.length - 1;
         return this.current();
     };
 
+    /**
+     * Сброс курсора в начальное положение
+     */
     Iterator.prototype.reset = function () {
         this._cursor = 0;
     };
 
+    /**
+     * Проверяет есть ли после текущего элемента еще один элемент
+     * @returns {boolean}
+     */
     Iterator.prototype.hasNext = function () {
         return this._cursor < this._items.length;
     };
 
+    /**
+     * Проверяет есть ли перед текущим элементом еще один элемент
+     * @returns {boolean}
+     */
     Iterator.prototype.hasPrevious = function () {
         return this._cursor > 0;
     };
 
+    /**
+     * Проходит по массиву и для каждого из его элементов выполняет функцию callback
+     * @param callback Функция, которая будет выполнена для каждого элемента
+     */
     Iterator.prototype.each = function (callback) {
         var item = this.first();
         for(; this.hasNext(); item = this.next()) {
