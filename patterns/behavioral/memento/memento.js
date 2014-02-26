641256017f0ea6f929b927b4460c4103faa119d2
instanceofpro <instanceofpro@gmail.com>
2014-02-26 12:12:30 +0600

    Добавил комментарии

--- patterns/behavioral/memento/memento.js
+++ patterns/behavioral/memento/memento.js
@@ -3,23 +3,45 @@
  * */
 var Person = (function () {
 
+    /**
+     * Создает экземпляр человека
+     * @param {string} name Имя человека
+     * @param {string} address Адрес местоположения человека
+     * @constructor
+     */
     function Person(name, address) {
         this.name = name;
         this.address = address;
     }
 
+    /**
+     * Меняет местоположение человека
+     * @param {string} newAddress Новое местоположение человека
+     */
     Person.prototype.move = function(newAddress) {
         this.address = newAddress;
     };
 
+    /**
+     * Возвращает текущее местопложение человека
+     * @returns {string}
+     */
     Person.prototype.whereami = function () {
         return this.address;
     };
 
+    /**
+     * Сохраняет текущее состояние человека в хранитель
+     * @returns {Memento}
+     */
     Person.prototype.save = function () {
         return new Memento(this);
     };
 
+    /**
+     * Откатывает состояние человека до состояния, которое хранится в memento
+     * @param memento
+     */
     Person.prototype.restore = function (memento) {
         if (memento === undefined) {
             throw new Error('Saved state is not found!');
@@ -38,10 +60,19 @@ var Person = (function () {
  * */
 var Memento = (function () {
 
+    /**
+     * Создает экземпляр мементо
+     * @param {*} state Состояние
+     * @constructor
+     */
     function Memento(state) {
         this.jsonState = JSON.stringify(state);
     }
 
+    /**
+     * Возвращает состояние
+     * @returns {*}
+     */
     Memento.prototype.getState = function () {
         return this.jsonState;
     };
@@ -54,19 +85,35 @@ var Memento = (function () {
  * */
 var Caretaker = (function (){
 
+    /**
+     * Создает экземпляр класса
+     * @constructor
+     */
     function Caretaker() {
         this.mementos = [];
     }
 
+    /**
+     * Сохраняет переданное в него состояние
+     * @param {string} memento Состояние
+     */
     Caretaker.prototype.set = function(memento) {
         this.mementos.push(memento);
     };
 
+    /**
+     * Возвращает сохраненное состояние по его индексу
+     * @param {number} index Индекс сохраненного состояния
+     * @returns {*}
+     */
     Caretaker.prototype.get = function(index) {
         var ind = parseInt(index, 10);
         return this.mementos[isNaN(ind) ? this.mementos.length - 1 : ind];
     };
 
+    /**
+     * Очищает список сохраненных состояний
+     */
     Caretaker.prototype.clean = function () {
         this.mementos = [];
     };
