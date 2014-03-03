735b8408c13696787022300a8ab0c1bf63676490
instanceofpro <instanceofpro@gmail.com>
2014-03-03 15:36:53 +0600

    Исправил реализацию паттерна Стратегия

--- patterns/behavioral/strategy/spec/strategy.spec.js
+++ patterns/behavioral/strategy/spec/strategy.spec.js
@@ -8,12 +8,11 @@ describe('Стратегия / Strategy', function () {
 
     describe('Инстанс', function () {
 
-        it('Strategy', function () {
+        it('Calculator', function () {
             expect(calc).to.be.instanceOf(Calculator);
         });
 
         it('Методы', function () {
-            expect(calc.add).to.be.a('function');
             expect(calc.set).to.be.a('function');
             expect(calc.execute).to.be.a('function');
         });
@@ -22,21 +21,20 @@ describe('Стратегия / Strategy', function () {
     describe('Поведение', function () {
 
         it('Calculator.execute', function () {
-            expect(calc.set('add').execute(4, 5)).to.equal(9);
-            expect(calc.set('subtract').execute(4, 5)).to.equal(-1);
-            expect(calc.set('multiply').execute(4, 5)).to.equal(20);
-            expect(calc.set('divide').execute(40, 5)).to.equal(8);
+            expect(calc.execute('add', 4, 5)).to.equal(9);
+            expect(calc.execute('subtract', 4, 5)).to.equal(-1);
+            expect(calc.execute('multiply', 4, 5)).to.equal(20);
+            expect(calc.execute('divide', 40, 5)).to.equal(8);
         });
 
-        it('Calculator.add', function () {
+        it('Calculator.set', function () {
 
             calc
-                .add('pow', function(x, y) {
+                .set('pow', function(x, y) {
                     return Math.pow(x, y);
-                })
-                .set('pow');
+                });
 
-            expect(calc.execute(15, 2)).to.equal(225);
+            expect(calc.execute('pow', 15, 2)).to.equal(225);
 
         });
 
--- patterns/behavioral/strategy/strategy.js
+++ patterns/behavioral/strategy/strategy.js
@@ -3,29 +3,53 @@
  * */
 var Calculator = (function () {
 
+    /**
+     * Создает экземпляр
+     * @constructor
+     */
     function Calculator() {
 
         /**
-         * Метод, который будет выполнять стратегия по умолчанию
-         * */
-        this._method = 'add';
-
-        /**
          * Встроенные алгоритмы
-         * */
+         * @private
+         */
         this._methods = {
+            /**
+             * Складывает два операнда
+             * @param {number} x Первый операнд
+             * @param {number} y Второй операнд
+             * @returns {number} Сумма
+             */
             add: function (x, y) {
                 return x + y;
             },
 
-            subtract: function(x, y) {
+            /**
+             * Вычитает из одного операнда другой
+             * @param {number} x Уменьшаемое
+             * @param {number} y Вычитаемое
+             * @returns {number} Разница
+             */
+            subtract: function (x, y) {
                 return x - y;
             },
 
+            /**
+             * Перемножает два операнда
+             * @param {number} x Первый операнд
+             * @param {number} y Второй операнд
+             * @returns {number} Произведение
+             */
             multiply: function (x, y) {
                 return x * y;
             },
 
+            /**
+             * Делит один операнд на другой
+             * @param {number} x Делимое
+             * @param {number} y Делитель
+             * @returns {number} Результат
+             */
             divide: function (x, y) {
                 return x / y;
             }
@@ -33,9 +57,12 @@ var Calculator = (function () {
     }
 
     /**
-     * Метод, который позволяет добавить новый метод в калькулятор
-     * */
-    Calculator.prototype.add = function (name, method) {
+     * Добавляет новый метод в калькулятор
+     * @param {string} name Имя метода
+     * @param {function} method Функция-метод
+     * @returns {Calculator}
+     */
+    Calculator.prototype.set = function (name, method) {
         if (this._methods[name] === undefined) {
             this._methods[name] = method;
         }
@@ -43,14 +70,19 @@ var Calculator = (function () {
         return this;
     };
 
-    Calculator.prototype.set = function (name) {
-        this._method = name;
-
-        return this;
-    };
-
-    Calculator.prototype.execute = function (x, y) {
-        return this._methods[this._method](x, y);
+    /**
+     * Вызывает метод и передает в него параметры
+     * @param {string} method Имя метода
+     * @param {number} x Первый операнд
+     * @param {number} y Второй операнд
+     * @returns {number} Результат
+     */
+    Calculator.prototype.execute = function (method, x, y) {
+        if (this._methods[method] !== undefined) {
+            return this._methods[method](x, y);
+        } else {
+            throw new Error('Method does not exists!');
+        }
     };
 
     return Calculator;
